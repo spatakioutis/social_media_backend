959de3ef0102dd03b5e85f9f62029052ecaf1824
sofok <sofoklis2314@gmail.com>
2024-06-19 15:17:02 +0300

    fix: bug where password was rehashed unintentionally and the account was no logner accessible

--- controllers/profilePicController.js
+++ controllers/profilePicController.js
@@ -6,10 +6,16 @@ const addProfilePic = async (req, res) => {
     const username = req.user.username
 
     try {
-        const image_url = await uploadFileToGoogleCS(username, req.file, 'profPics')
-        
+
         const user = await User.findOne({ username })
-        console.log(user)
+
+        // delete previous profilePic if it was not the default
+        if ( user.profilePic !== 'https://storage.googleapis.com/spatakioutis_app_img/profilePics/default_pic.png') {
+            await deleteFileFromGoogleCS(filepath[filepath.length - 1], 'profPics')
+        }
+        
+        const image_url = await uploadFileToGoogleCS(username, req.file, 'profPics')
+
         user.profilePic = image_url
         await user.save()
 
@@ -32,6 +38,7 @@ const deleteProfilePic = async (req, res) => {
         const user = await User.findOne({ username })
 
         const filepath = user.profilePic.split('/')
+        if ( filepath[filepath.length - 1] !== 'default')
 
         await deleteFileFromGoogleCS(filepath[filepath.length - 1], 'profPics')
         
--- models/User.js
+++ models/User.js
@@ -12,8 +12,14 @@ const userSchema = new mongoose.Schema({
 })
 
 userSchema.pre('save', async function (next) {
-    this.password = await bcrypt.hash(this.password, 10)
-    next()
+    try {
+        if (this.isModified('password') || this.isNew) {
+            this.password = await bcrypt.hash(this.password, 10)
+        }
+        next()
+    } catch (err) {
+        next(err)
+    }
 })
 
 const User = mongoose.model('User', userSchema, 'users')
